773016f6486cf84d1db44b127780f697ab665756
authentiano <emmasanauthentiano@gmail.com>
2026-03-09 10:08:24 -0400

    created the viewsets

--- backend/members/views.py
+++ backend/members/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from . models import Member
+from . serializer import MemberSerializer
+from accounts.permissions import isAdministrator, isSuperAdmin
+
 
 # Create your views here.
+
+class MemberViewSet(ModelViewSet):
+    queryset = Member.objects.all()
+    serializer_class = MemberSerializer
+    permission_classes = [isAdministrator | isSuperAdmin]
+
+    
